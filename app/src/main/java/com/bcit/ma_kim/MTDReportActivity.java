df012a12bc5aa66f3ca0dd8a5561eb6d05591a8c
Sehyung (Eric) Kim <kimeric97@hotmail.com>
2020-11-13 14:52:26 -0800

    commiiiiiiiiiiit

---
+++ app/src/main/java/com/bcit/ma_kim/MTDReportActivity.java
@@ -0,0 +1,110 @@
+package com.bcit.ma_kim;
+
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MTDReportActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference().child("BPReadings");
+
+    ArrayList<BloodPressureReadingActivity> bpReadingsList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_"MTDREPORT");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot snapshot: dataSnapshot.getChildren()){
+                    BloodPressureReadingActivity bpReading = snapshot.getValue(BloodPressureReadingActivity.class);
+                    bpReadingsList.add(bpReading);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        /*
+        Placeholder "button" to generate report
+         */
+        Button genReportBtn = findViewById(R.id.btnGenReport);
+        genReportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                generateReport(view);
+            }
+        });
+    }
+
+    public void generateReport(View view){
+        Spinner chosenSpinnerUser = (Spinner)findViewById(R.id."spinnerID");
+        String spinnerUser = chosenSpinnerUser.getSelectedItem().toString();
+
+        //used for calculating averages
+        int systolicTotal = 0;
+        int diastolicTotal = 0;
+        int counter = 0;
+        //calculated averages
+        double systolicAverage;
+        double diastolicAverage;
+        String conditionAverage;
+
+        for(BloodPressureReadingActivity bpReading: bpReadingsList){
+            if(bpReading.spUser.equals(spinnerUser)){
+                systolicTotal = systolicTotal + Integer.parseInt(bpReading.systolicReading);
+                diastolicTotal = diastolicTotal + Integer.parseInt(bpReading.diastolicReading);
+                counter++;
+            }
+        }
+
+        //Assign the calculated average
+        systolicAverage = systolicTotal/counter;
+        diastolicAverage = diastolicTotal/counter;
+
+        if(systolicAverage > 180 || diastolicAverage > 120){
+            conditionAverage = ConditionTypes.HYPERTENSIVE.toString();
+        } else if(systolicAverage >= 140 || diastolicAverage >= 90){
+            conditionAverage = ConditionTypes.STAGE2.toString();
+        } else if(systolicAverage >= 130 || diastolicAverage >= 80){
+            conditionAverage = ConditionTypes.STAGE1.toString();
+        } else if(systolicAverage >= 120){
+            conditionAverage = ConditionTypes.ELEVATED.toString();
+        } else {
+            conditionAverage = ConditionTypes.NORMAL.toString();
+        }
+
+        /*
+        Placeholder textviews to output these results
+         */
+        TextView tvSystolicAvg = findViewById(R.id."textViewSystolicAve");
+        TextView tvDiastolicAvg = findViewById(R.id."textViewDiastolicAve");
+        TextView tvConditionAvg = findViewById(R.id."textViewConditionAve");
+
+        tvSystolicAvg.setText(Double.toString(systolicAverage));
+        tvDiastolicAvg.setText(Double.toString(diastolicAverage));
+        tvConditionAvg.setText(conditionAverage);
+
+    }
+}
--- app/src/main/java/com/bcit/ma_kim/MainActivity.java
+++ app/src/main/java/com/bcit/ma_kim/MainActivity.java
@@ -1,20 +1,128 @@
 package com.bcit.ma_kim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference dbRef = database.getReference().child("BPReadings");
+
+    ArrayList<BloodPressureReadingActivity> bpReadingsList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mDatabase.setValue("Hello World");
+
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+        }
+
+        setContentView(R.layout.activity_main);
+
+        dbRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                bpReadingsList.clear();
+
+                for (DataSnapshot studentSnapshot : dataSnapshot.getChildren()) {
+                    BloodPressureReadingActivity bpReading = studentSnapshot.getValue(BloodPressureReadingActivity.class);
+                    bpReadingsList.add(bpReading);
+                }
+
+                //Function to be implemented to display the readings
+                    displayReadings(bpReadingsList);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        /*
+        Placeholder button to link to month-to-date report activity for now
+         */
+        Button btnGoToMTDReport = findViewById(R.id."btnGoToMTDActivity");
+        btnGoToMTDReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MTDReportActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        /*
+        Placeholder button to add
+         */
+        Button addReportBtn = findViewById(R.id."btnAddReport");;
+        addReportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addReading();
+            }
+        });
+
     }
-}
+
+    private void addReading() {
+    }
+
+    //to be implemented
+    private void displayReadings(ArrayList<BloodPressureReadingActivity> bpReadingsList){
+        Spinner spText = (Spinner)findViewById(R.id."spinnerID");
+        String spUser = spText.getSelectedItem().toString();
+
+        EditText editText;
+        editText = findViewById(R.id."textViewSystolic");
+        String systolicReading = editText.getText().toString();
+        editText.setText("");
+
+        editText = findViewById(R.id."textViewDiastolic");
+        String diastolicReading = editText.getText().toString();
+        editText.setText("");
+
+
+        //creates new reading from input data
+        BloodPressureReadingActivity bpReading = new BloodPressureReadingActivity(spUser,
+                systolicReading,
+                diastolicReading);
+
+        //*TO BE IMPLEMENTED -> MAKE IT STAY ON SCREEN
+        if (bpReading.condition.equals("HYPERTENSIVE")) {
+            Toast.makeText(MainActivity.this,
+                    "WARNIRNINGINIIG",
+                    Toast.LENGTH_SHORT).show();
+        }
+        Task setValueTask = dbRef.child(bpReading.id).setValue(bpReading);
+
+        setValueTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this,
+                        getString("erororororoorrroror") + e.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    }
