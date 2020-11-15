996318217f319c41d833c1795a27e17b1957ea0b
Sehyung (Eric) Kim <kimeric97@hotmail.com>
2020-11-14 18:19:13 -0800

    Added time to show within entries

--- app/src/main/java/com/bcit/ma_kim/RelativeAdapter.java
+++ app/src/main/java/com/bcit/ma_kim/RelativeAdapter.java
@@ -12,6 +12,8 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
@@ -49,12 +51,14 @@ public class RelativeAdapter extends RecyclerView.Adapter<RelativeAdapter.ViewHo
         TextView sys_reading = cardView.findViewById(R.id.sys_reading);
         TextView dia_reading = cardView.findViewById(R.id.dia_reading);
         TextView date = cardView.findViewById(R.id.date_entered);
+        TextView time = cardView.findViewById(R.id.time_entered);
         TextView condition = cardView.findViewById(R.id.cond_reading);
         TextView clear = cardView.findViewById(R.id.tx_clear);
 
         sys_reading.setText(cardView.getResources().getText(R.string.systolic_readings) + ": " + data[position].getSystolicReading());
         dia_reading.setText(cardView.getResources().getText(R.string.diastolic_reading) + ": " + data[position].getDiastolicReading());
         date.setText(cardView.getResources().getText(R.string.reading_date) + ": " + data[position].getDate());
+        time.setText(cardView.getResources().getText(R.string.time) + ": " + data[position].getTime());
         condition.setText(cardView.getResources().getText(R.string.condition_reading) + ": " + data[position].getCondition());
         clear.setText(R.string.clearText);
 
@@ -67,28 +71,6 @@ public class RelativeAdapter extends RecyclerView.Adapter<RelativeAdapter.ViewHo
                 cardView.getContext().startActivity(i);
             }
         });
-
-/*
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseDatabase.getInstance().getReference()
-                        .child(RelativeAdapter.this.relative.getTitle()).child("id".toString()).removeValue()
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    Log.d("Delete", "Report has been deleted");
-                                } else {
-                                    Log.d("Delete", "Report couldn't be deleted");
-                                }
-                            }
-                        });
-            }
-        });
-
- */
-
     }
 
     @Override
